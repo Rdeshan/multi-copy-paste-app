930f2a777a5ecbd3d79f94cf368ae58617b0b7d4
Ravindu Deshan Perera <deshr.perera03@gmail.com>
2026-01-01 23:10:53 +0530

    first code (just samples)

--- main.js
+++ main.js
@@ -1,42 +1,22 @@
-const { app, BrowserWindow, globalShortcut, clipboard } = require('electron')
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 let mainWindow
-let slots = {} // {1: "text", 2: "text"}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 300,
-    height: 200,
-    show: false // background app
+    width: 500,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
+
+  mainWindow.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app.whenReady().then(createWindow)
 
-  // COPY to slot 1
-  globalShortcut.register('Control+Shift+1', () => {
-    slots[1] = clipboard.readText()
-    console.log('Saved to slot 1')
-  })
-
-  // COPY to slot 2
-  globalShortcut.register('Control+Shift+2', () => {
-    slots[2] = clipboard.readText()
-    console.log('Saved to slot 2')
-  })
-
-  // PASTE slot 1
-  globalShortcut.register('Control+Alt+1', () => {
-    if (slots[1]) clipboard.writeText(slots[1])
-  })
-
-  // PASTE slot 2
-  globalShortcut.register('Control+Alt+2', () => {
-    if (slots[2]) clipboard.writeText(slots[2])
-  })
-})
-
-app.on('will-quit', () => {
-  globalShortcut.unregisterAll()
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
 })
